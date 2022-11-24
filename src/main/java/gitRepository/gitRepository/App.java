720a3bdfaba2917b7031670a80e47b4ee6ce3317
Santosh Koirala <santoshkoirala17@gmail.com>
2022-11-24 12:08:44 -0600

    first

---
+++ src/main/java/gitRepository/gitRepository/App.java
@@ -0,0 +1,13 @@
+package gitRepository.gitRepository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
